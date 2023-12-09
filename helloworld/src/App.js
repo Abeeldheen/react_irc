a1f84890a10730aee06855750ffb55a54bb64378
Abeeldheen <153354615+Abeeldheen@users.noreply.github.com>
2023-12-09 14:14:09 +0530

    updated CE 1

---
+++ helloworld/src/App.js
@@ -0,0 +1,12 @@
+import Sample from "./Sample";
+import './App.css'
+function App()
+{
+  return(
+    <div>
+      <sample></sample>
+      <h1>HELLO WORLD</h1>
+    </div>  
+  )
+}
+export default App;
